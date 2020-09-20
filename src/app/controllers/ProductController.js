b078c7e761dea174c45b9b3aa355df62cc68d1a4
Erick <erick.wdev@gmail.com>
2020-09-20 12:26:27 -0300

    estilizaçao de atualizaçao e formataçao numerica BRL

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,9 +1,10 @@
+const { formatPrice} = require('../../lib/utils')
+
 const Category = require('../models/Category')
 const Product = require('../models/Product')
 
 module.exports = {
   create(req, res) {
-    //Pegar Categorias
     Category.all()
     .then(function(results) {
 
@@ -13,25 +14,36 @@ module.exports = {
     }).catch(function(err) { 
       throw new Error(err)
     })
-
   },
   async post(req, res) {
-  //Logica de Salvar
+
    const keys = Object.keys(req.body)
 
     for(key of keys) {
     if(req.body[key] == "") {
         return res.send('Please, fill all the fields!')
-
         }
     }
     let results = await Product.create(req.body)
     const productID = results.rows[0].id
 
+    return res.redirect(`/products/${productID }`)
+    
+  },
+  async edit(req, res) {
+
+    let results = await Product.find(req.params.id)
+    const product = results.rows[0]
+
+    if(!product) return res.send("Product not found")
+
+    product.old_price = formatPrice(product.old_price)
+    product.price = formatPrice(product.price)
+
     results = await Category.all()
-    const categories = results.rows
+    const categories = results.rows 
 
     
-    return res.render("products/create.njk", { productID, categories })
+    return res.render("products/edit.njk", { product, categories })
   }
 }
