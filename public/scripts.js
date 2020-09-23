f6d85d7017fcc4d2889acaf6a20256a986869172
Erick <erick.wdev@gmail.com>
2020-09-23 12:31:31 -0300

    Limpeza e reorganizaçao de codigo

--- public/scripts.js
+++ public/scripts.js
@@ -15,16 +15,10 @@ const Mask = {
 }
 
 const PhotosUpload = {
+  preview: document.querySelector('#photos-preview'), 
   uploadLimit: 6,
   handleFileInput(event) {
     const { files: fileList} = event.target
-    const { uploadLimit } = PhotosUpload
-
-    if (fileList.length > uploadLimit) {
-      alert(`Envie no máximo ${uploadLimit} fotos`)
-      event.preventDefault()
-      return
-    }
 
     Array.from(fileList).forEach(file => {
       const reader = new FileReader()
@@ -32,18 +26,34 @@ const PhotosUpload = {
       reader.onload = () => {
         const image = new Image()
         image.src = String(reader.result)
-
-        const div = document.createElement('div')
-        div.classList.add('photo')
-
-        div.onclick = () => alert('remover foto')
-
-        div.appendChild(image)
-
-        document.querySelector('#photos-preview').appendChild(div)
+        
+        const div = PhotosUpload.getContainer(image)
+        PhotosUpload.preview.appendChild(div)
       }
 
       reader.readAsDataURL(file)
     })
+  },
+  hasLimit(event) {
+    const { uploadLimit } = PhotosUpload
+
+    if(PhotosUpload.hasLimit(event)) return
+
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+      event.preventDefault()
+      return true
+    }
+    return false
+  },
+  getContainer(image) {
+    const div = document.createElement('div')
+      div.classList.add('photo')
+
+      div.onclick = () => alert('remover foto')
+
+      div.appendChild(image)
+
+      return div
   }
 }
