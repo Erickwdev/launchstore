856a82bddd4a6978f2d7c748d2b09dbc5a7166ee
Erick <erick.wdev@gmail.com>
2020-09-23 14:28:13 -0300

    removendo imagens do front-end

--- public/scripts.js
+++ public/scripts.js
@@ -20,8 +20,11 @@ const PhotosUpload = {
   handleFileInput(event) {
     const { files: fileList} = event.target
 
+    if(PhotosUpload.hasLimit(event)) return
+
     Array.from(fileList).forEach(file => {
       const reader = new FileReader()
+
 
       reader.onload = () => {
         const image = new Image()
@@ -36,24 +39,39 @@ const PhotosUpload = {
   },
   hasLimit(event) {
     const { uploadLimit } = PhotosUpload
-
-    if(PhotosUpload.hasLimit(event)) return
+    const { files: fileList } = event.target
 
     if (fileList.length > uploadLimit) {
       alert(`Envie no máximo ${uploadLimit} fotos`)
       event.preventDefault()
       return true
     }
+
     return false
   },
   getContainer(image) {
     const div = document.createElement('div')
       div.classList.add('photo')
 
-      div.onclick = () => alert('remover foto')
+      div.onclick = PhotosUpload.removePhoto
 
       div.appendChild(image)
 
+      div.appendChild(PhotosUpload.getRemoveButton())
+
       return div
+  },
+  getRemoveButton() {
+    const button = document.createElement('i')
+    button.classList.add('material-icons')
+    button.innerHTML = "close"
+    return button
+  },
+  removePhoto(event) {
+    const photoDiv = event.target.parentNode
+    const photoArray = Array.from(PhotosUpload.preview.children)
+    const index = photoArray.indexOf(photoDiv)
+
+    photoDiv.remove()
   }
 }
