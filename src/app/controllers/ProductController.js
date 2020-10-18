3d13f81ad089f2834c7ae2224c4068e6eb62651f
Erick <erick.wdev@gmail.com>
2020-10-18 15:32:12 -0300

    atualizando datas

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,4 +1,4 @@
-const { formatPrice} = require('../../lib/utils')
+const { formatPrice, date} = require('../../lib/utils')
 
 const Category = require('../models/Category')
 const Product = require('../models/Product')
@@ -37,8 +37,24 @@ module.exports = {
     return res.redirect(`/products/${productID}/edit`)
     
   },
-  show(req, res) {
-    return res.render("products/show")
+  async show(req, res) {
+
+    let results = await Product.find(req.params.id)
+    const product = results.rows[0]
+
+    if(!product) return res.send("Product Not Found!")
+
+    const { day, hour, minutes, month} = date(product.updated_at)
+
+    product.published = {
+      day: `${day}/${month}`,
+      hour: `${hour}h${minutes}`,
+    }
+
+    product.oldPrice = formatPrice(product.old_price)
+    product.price = formatPrice(product.price)
+
+    return res.render("products/show", { product })
   },
   async edit(req, res) {
 
